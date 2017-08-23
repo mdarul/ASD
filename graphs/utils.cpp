be1eddaf51294d7453eb4fd6fb6bbb1b4ed643b6
mdarul@outlook.com <mdarul618@gmail.com>
2017-08-23 14:21:19 +0100

    added graphs representation and some basic functions

---
+++ graphs/utils.cpp
@@ -0,0 +1,110 @@
+//
+// Created by michal on 23/08/17.
+//
+
+#include <iostream>
+#include "utils.h"
+
+vertex **create_directed_list(int v, int e)
+{
+    vertex **G = new vertex *[v];
+    for(int i=0; i<v; i++) G[i] = NULL;
+
+    int val1, val2;
+    for(int i=0; i<e; i++)
+    {
+        std::cin >> val1 >> val2;
+
+        vertex *new_vertex = new vertex;
+        new_vertex->value = val2;
+        new_vertex->next = G[val1];
+        G[val1] = new_vertex;
+    }
+
+    return G;
+}
+
+vertex **create_undirected_list(int v, int e)
+{
+    vertex **G = new vertex *[v];
+    for(int i=0; i<v; i++) G[i] = NULL;
+
+    int val1, val2;
+    for(int i=0; i<e; i++)
+    {
+        std::cin >> val1 >> val2;
+
+        vertex *new_vertex1 = new vertex;
+        new_vertex1->value = val2;
+        new_vertex1->next = G[val1];
+        G[val1] = new_vertex1;
+
+        vertex *new_vertex2 = new vertex;
+        new_vertex2->value = val1;
+        new_vertex2->next = G[val2];
+        G[val2] = new_vertex2;
+    }
+
+    return G;
+}
+
+int **create_directed_matrix(int v, int e)
+{
+    int **G = new int *[v];
+    for(int i=0; i<v; i++) G[i] = new int[v];
+    for(int i=0; i<v; i++)
+        for(int j=0; j<v; j++)
+            G[i][j] = 0;
+
+    int val1, val2;
+    for(int i=0; i<e; i++)
+    {
+        std::cin >> val1 >> val2;
+        G[val1][val2] = 1;
+    }
+
+    return G;
+}
+
+int **create_undirected_matrix(int v, int e)
+{
+    int **G = new int *[v];
+    for(int i=0; i<v; i++) G[i] = new int[v];
+    for(int i=0; i<v; i++)
+        for(int j=0; j<v; j++)
+            G[i][j] = 0;
+
+    int val1, val2;
+    for(int i=0; i<e; i++)
+    {
+        std::cin >> val1 >> val2;
+        G[val1][val2] = G[val2][val1] = 1;
+    }
+
+    return G;
+}
+
+void display_list(vertex **G, int v)
+{
+    vertex *tmp;
+    for(int i=0; i<v; i++)
+    {
+        tmp = G[i];
+        std::cout << i <<": ";
+        while(tmp)
+        {
+            std::cout << tmp->value << " ";
+            tmp = tmp->next;
+        }
+        std::cout << std::endl;
+    }
+}
+
+void display_matrix(int **G, int v)
+{
+    for(int i=0; i<v; i++)
+    {
+        for(int j=0; j<v; j++) std::cout << G[i][j] << " ";
+        std::cout << std::endl;
+    }
+}
---
+++ graphs/utils.h
@@ -0,0 +1,21 @@
+//
+// Created by michal on 23/08/17.
+//
+
+#ifndef GRAPHS_UTILS_H
+#define GRAPHS_UTILS_H
+
+struct vertex
+{
+    int value;
+    vertex *next;
+};
+
+vertex **create_directed_list(int v, int e); // v - vertices, e - edges
+vertex **create_undirected_list(int v, int e);
+int **create_directed_matrix(int v, int e);
+int **create_undirected_matrix(int v, int e);
+void display_list(vertex **G, int v);
+void display_matrix(int **G, int v);
+
+#endif //GRAPHS_UTILS_H
